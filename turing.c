a181a3d4d113542d4f05fbaf6832750a38b16515
JingwuWang <861349061@qq.com>
2019-12-04 13:53:56 +0800

    parse

--- turing.c
+++ turing.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include "turing.h"
+struct Turing_machine* turing_machine;
+void parse(char *buf);
 
 
 int main(int argc,char **args)
@@ -8,15 +11,18 @@ int main(int argc,char **args)
 	{
 		char *s="/test.tm";
 		s=strcat(args[1],s);
-		FILE *fp=fopen("w+",s);
-		char buf[3000];
-		printf("%s\n",s);
+		FILE *fp=fopen(s,"r+");
+		char buf[5000];
+
+		turing_machine=(struct Turing_machine*)malloc(sizeof(struct Turing_machine));
 		while(!feof(fp))
-    {
-		printf("1\n");
-        fgets(buf,10,fp);
-		printf("%s\n",buf);
-    }
+    	{
+        	fgets(buf,4900,fp);
+			//parse
+
+			parse(buf);
+    	}
+		
 	}
 	else
 	{
@@ -24,3 +30,152 @@ int main(int argc,char **args)
 	}
 }
 
+void parse(char *buf)
+{
+	if(buf[0]=='#')
+	{
+		//状态集
+		if(buf[1]=='Q')
+		{
+			int l;
+			for(l=2;buf[l]!='{';l++);
+			int r1,r2=l;
+			for(r1=l;;r1++)
+			{
+				if(buf[r1]==','||buf[r1]=='}')
+				{
+					int len=r1-r2;
+					char *state=(char *)malloc(sizeof(char)*(len+5));
+					for(int i=0;i<len-1;i++)
+					{
+						state[i]=buf[r2+i+1];
+					}
+					struct symbol_set *t=create_symbol(state);
+					t->next=turing_machine->state_set;
+					turing_machine->state_set=t;
+					r2=r1;
+					if(buf[r1]=='}')
+					{
+						break;
+					}
+				}
+			}
+		}
+		//输出符号集
+		if(buf[1]=='S')
+		{
+			int l;
+			for(l=2;buf[l]!='{';l++);
+			int r1,r2=l;
+			for(r1=l;;r1++)
+			{
+				if(buf[r1]==','||buf[r1]=='}')
+				{
+					int len=r1-r2;
+					char *state=(char *)malloc(sizeof(char)*(len+5));
+					for(int i=0;i<len-1;i++)
+					{
+						state[i]=buf[r2+i+1];
+					}
+					struct symbol_set *t=create_symbol(state);
+					t->next=turing_machine->input_symbol_set;
+					turing_machine->input_symbol_set=t;
+					r2=r1;
+					if(buf[r1]=='}')
+					{
+						break;
+					}
+				}
+			}
+		}
+		//纸带符号集
+		if(buf[1]=='G')
+		{
+			int l;
+			for(l=2;buf[l]!='{';l++);
+			int r1,r2=l;
+			for(r1=l;;r1++)
+			{
+				if(buf[r1]==','||buf[r1]=='}')
+				{
+					int len=r1-r2;
+					char *state=(char *)malloc(sizeof(char)*(len+5));
+					for(int i=0;i<len-1;i++)
+					{
+						state[i]=buf[r2+i+1];
+					}
+					struct symbol_set *t=create_symbol(state);
+					t->next=turing_machine->tape_symbol_set;
+					turing_machine->tape_symbol_set=t;
+					r2=r1;
+					if(buf[r1]=='}')
+					{
+						break;
+					}
+				}
+			}
+		}
+		//终结状态集
+		if(buf[1]=='F')
+		{
+			int l;
+			for(l=2;buf[l]!='{';l++);
+			int r1,r2=l;
+			for(r1=l;;r1++)
+			{
+				if(buf[r1]==','||buf[r1]=='}')
+				{
+					int len=r1-r2;
+					char *state=(char *)malloc(sizeof(char)*(len+5));
+					for(int i=0;i<len-1;i++)
+					{
+						state[i]=buf[r2+i+1];
+					}
+					struct symbol_set *t=create_symbol(state);
+					t->next=turing_machine->terminate_symbol_set;
+					turing_machine->terminate_symbol_set=t;
+					r2=r1;
+					if(buf[r1]=='}')
+					{
+						break;
+					}
+				}
+			}
+		}
+		if(buf[1]=='B')
+		{
+			char *space=(char *)malloc(sizeof(char)*3);
+			space[0]=buf[5];
+			turing_machine->space_symbol=space;
+		}
+		if(buf[1]=='q'&&buf[2]=='0')
+		{
+			int l=6,r;
+			for(r=l;buf[r];r++);
+			int len=r-l;
+			char *init_s=(char *)malloc(sizeof(char)*len+5);
+			for(int i=0;i<len-1;i++)
+			{
+				init_s[i]=buf[l+i];
+			}
+			turing_machine->init_state=init_s;
+		}
+		if(buf[1]=='N')
+		{
+			int l=5,r;
+			for(r=l;buf[r];r++);
+			int len=r-l;
+			char *init_s=(char *)malloc(sizeof(char)*len+5);
+			for(int i=0;i<len-1;i++)
+			{
+				init_s[i]=buf[l+i];
+			}
+			turing_machine->tape_number=atoi(init_s);
+		}
+	}
+	else if(buf[0]!=';')
+	{
+		
+	}
+	
+}
---
+++ turing.h
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+struct symbol_set{
+	char *element;
+	struct symbol_set* next;
+};
+
+struct transform_function{
+    char *current_state;
+    char *next_state;
+    char *current_symbol;
+    char *move_direction;
+    char *next_symbol;
+    struct transform_function *next;
+};
+
+struct Turing_machine{
+    struct symbol_set *state_set;                     //状态集
+    struct symbol_set *input_symbol_set;              //输入符号集
+    struct symbol_set *tape_symbol_set;               //纸带符号集
+    char *init_state;                                 //初始状态
+    char *space_symbol;                               //空格符号
+    struct symbol_set *terminate_symbol_set;          //终结状态集
+    int tape_number;                                  //纸带数
+    struct transform_function* transform_function;    //转移函数
+}  ;
+
+struct transform_function* create_transform_function(char *current_state,char *current_symbol,char *next_symbol,char *move_direction,char *next_state)
+{
+    struct transform_function* tf=(struct symbol_set*)malloc(sizeof(struct symbol_set));
+    tf->current_state=current_state;
+    tf->current_symbol=current_symbol;
+    tf->next_state=next_state;
+    tf->next_symbol=next_symbol;
+    tf->move_direction=move_direction;
+    return tf;
+}
+
+struct symbol_set* create_symbol(char *s)
+{
+    struct symbol_set* ss=(struct symbol_set*)malloc(sizeof(struct symbol_set));
+    ss->element=s;
+    return ss;
+}
